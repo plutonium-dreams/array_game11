ccc4d713c71ea9b85bc0e9b85b304addc3c96e3a
Joeh3 <josefjaen8@gmail.com>
2026-04-24 01:08:44 +0800

    Update market.py

--- market.py
+++ market.py
@@ -1,3 +1,36 @@
 '''
 market engine
+
+implement with pure numpy first before graphing with python
 '''
+import matplotlib.pyplot as plt
+import numpy as np
+from defaults import *
+from utils import *
+
+class Trend():
+    def __init__(self, a, b, c):
+        '''
+        - direction on market
+        - strength on market
+        - variations
+        '''
+        self.a = a
+
+class Market():
+    def __init__(self):
+        self.mean = 0
+        self.std = 0
+
+        self.velocity = 0
+        self.acceleration = 0
+    
+    def forecast(self):
+        pass
+
+    def update(self):
+        pass
+
+    def gen_points(self):
+        pass
+
